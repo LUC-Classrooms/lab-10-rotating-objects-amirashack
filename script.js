54a8fc8b8bff35880de7ef4cb61599773149be98
amirashack <156958322+amirashack@users.noreply.github.com>
2024-03-24 17:04:21 +0000

    Lab 10 Submitted

--- script.js
+++ script.js
@@ -34,6 +34,8 @@ function draw() {
     if(mouseIsPressed){
       // spin object 1 by calling its spin() method
       // you will have to create that method inside the myClass constructor ...
+
+      obj1.spin(.05);
     } else {
       obj1.reset(); // the reset function is done for you
     }
@@ -41,6 +43,7 @@ function draw() {
   if(mouse2 < 50) {
     if(mouseIsPressed){
       // spin object 2 by calling its spin() method
+      obj2.spin(-.05);
     } else {
       obj2.reset();
     }
@@ -64,13 +67,19 @@ function myClass(tempX, tempY){
   Create a spin method here. Look at the reset() method below and the spin() method from the Sakai example for this week.
   the function assigned to this.spin should take a number argument; that number should be _added_ to the value of this.angle. The reset() method sets this.angle back to 0, and this.scale back to 1
   */
-  
+  this.spin = function(s){
+    this.angle += s;
+    this.scale -= .01;
+
+  }
+
   this.reset = function(){
       this.angle = 0;
       this.scale = 1;
   }
   
   this.display = function(){
+    
 /*
   Make some significant changes here. Remember to push(), translate(), and rotate() before drawing. Don't forget to pop() afterwards!
   
@@ -78,12 +87,16 @@ function myClass(tempX, tempY){
   
   The argument to rotate() should be "this.angle", which is affected by the spin() and reset() methods
 */
-    
+    push(); //creates a transparency layer
+    translate(this.x, this.y);
+    rotate(this.angle);
+    scale(this.scale);
     fill(this.color);
     rectMode(CENTER);
     // change the location to 0, 0
-    rect(this.x, this.y, this.diam, this.diam);
+    rect(0, 0, this.diam, this.diam); //draw a square
     
+    pop(); //disposes of the layer
   }
   
 }
